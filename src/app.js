644139ff066f51952288bdf8e6f6f1d8bcf6d9a3
acidicneko <ayush7788899@gmail.com>
2024-12-12 11:51:43 +0530

    fix: continue with google button on create an account page

--- src/app.js
+++ src/app.js
@@ -4,27 +4,30 @@ import { app } from "./firebase/firebaseConfig";
 
 const auth = getAuth(app);
 
-const signInBtn = document.getElementById('signInWithGoogleBtn');
+const signInBtns = document.querySelectorAll(".button-google");
 
 const provider = new GoogleAuthProvider();
 
 setPersistence(auth, browserLocalPersistence)
   .then(() => {
-    signInBtn.onclick = () => {
-      signInWithPopup(auth, provider)
-        .then((result) => {
-          const user = result.user;
-          console.log("User signed in:", user.displayName);
-          window.location.href = "dashboard.html";
-        })
-        .catch((error) => {
-          console.error("Sign-in error:", error.message);
-        });
-    };
+    signInBtns.forEach((button) => {
+      button.onclick = () => {
+        signInWithPopup(auth, provider)
+          .then((result) => {
+            const user = result.user;
+            console.log("User signed in:", user.displayName);
+            window.location.href = "dashboard.html";
+          })
+          .catch((error) => {
+            console.error("Sign-in error:", error.message);
+          });
+      };
+    });
   })
   .catch((error) => {
     console.error("Error setting persistence:", error.message);
   });
+
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
