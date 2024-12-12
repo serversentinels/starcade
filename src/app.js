e13844dbeac518b054a84ff903bf8c486d08a4f2
acidicneko <ayush7788899@gmail.com>
2024-12-12 13:52:46 +0530

    add: save user data to firestore upon account creation

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import { getAuth, setPersistence, browserLocalPersistence, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "firebase/auth";
+import { getFirestore, setDoc, doc } from "firebase/firestore";
 
 import { app } from "./firebase/firebaseConfig";
 
 const auth = getAuth(app);
-
+const db = getFirestore(app);
 const signInBtns = document.querySelectorAll(".button-google");
 
 const provider = new GoogleAuthProvider();
@@ -13,10 +14,26 @@ setPersistence(auth, browserLocalPersistence)
     signInBtns.forEach((button) => {
       button.onclick = () => {
         signInWithPopup(auth, provider)
-          .then((result) => {
+          .then(async (result) => {
             const user = result.user;
             console.log("User signed in:", user.displayName);
-            window.location.href = "dashboard.html";
+
+            const userData = {
+              name: user.displayName,
+              email: user.email,
+              uid: user.uid,
+              exp: 0,
+              provider: "google"
+            };
+            console.log("Trying to write user data to Firestore");
+            const docRef = doc(db, "users", user.uid);
+            await setDoc(docRef, userData).then(() => {
+              console.log('User data written to Firestore');
+            })
+              .catch((error) => {
+                console.error("Error writing document: ", error);
+              });
+
           })
           .catch((error) => {
             console.error("Sign-in error:", error.message);
